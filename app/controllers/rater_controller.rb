8f2454da84d1a1e07a75eff7ea259dfbfdbb4920
Anbazhagan <anbublacky@gmail.com>
2015-11-12 20:08:46 +0530

    ajaxified average rating

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -4,8 +4,13 @@ class RaterController < ApplicationController
     if logged_in?
       obj = params[:klass].classify.constantize.find(params[:id])
       obj.rate params[:score].to_i, @current_user.id.to_s, params[:dimension]
-
-      render :json => true
+      @object = obj
+      Rails.logger.info "----------------------------------------------------------------------"
+      Rails.logger.info @object.class
+      # render :json => true
+      respond_to do |format|
+        format.js { render layout: false}
+      end      
     else
       render :json => false
     end
